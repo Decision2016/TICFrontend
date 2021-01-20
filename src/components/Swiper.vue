a0ec9f43a5668d06d5397d5f2c1904099a6ca3b9
decision2016 <787362202@qq.com>
2021-01-20 22:53:50 +0800

    feat: Swiper and Avator added

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="swiper">
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div id="carousel" class="carousel slide carousel" data-bs-ride="carousel">
       <ol class="carousel-indicators">
         <li data-bs-target="#carouselExampleCaptions" v-for="(item, index) in urls"  :data-bs-side-to="index" :class="index===0?'active':''" :key="index"></li>
       </ol>
       <div class="carousel-inner">
-        <div v-for="(item, index) in urls" :class="index===0?'carousel-item active':'carousel-item'" :key="index">
+        <div v-for="(item, index) in urls" :class="index===0?'carousel-item active':'carousel-item'" :key="index" style="position: relative">
+          <div class="blur" :style="{'background': 'url('+item+')'}" ></div>
           <img id="show-images" :src="item" alt="...">
-          <div class="carousel-caption d-none d-md-block">
+          <div class="carousel-caption d-none d-md-block" >
             <p>{{descriptions[index]}}</p>
           </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+      <a class="carousel-control-prev" href="#carousel" role="button" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </a>
-      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+      <a class="carousel-control-next" href="#carousel" role="button" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </a>
@@ -37,13 +38,17 @@ export default {
 </script>
 
 <style scoped>
-  .swiper {
-    max-height: 700px;
-    width: 100%;
+
+  #show-images{
+    position: absolute;
+    width: 1200px;
+    height: 500px;
+    transform: translate(-50%, -110%);
   }
 
-  #show-images {
+  .blur {
     width: 100%;
-    max-height: 670px;
+    height: 600px;
+    filter: blur(100px);
   }
 </style>
--- src/views/front/home.vue
+++ src/views/front/home.vue
@@ -4,14 +4,30 @@
       <Navbar></Navbar>
       <Swpier :urls="urls" :descriptions="descriptions"></Swpier>
     </div>
-    <div class="home-articles">
-      <div class="home-article" v-for="(item, index) in articles" :key="index">
-        <div :class="index%2===0?'home-article-right':'home-article-left'">
-          
+    <!--div class="home-articles">
+    </div-->
+    <div class="home-personnel">
+      <h1 style="margin-top: 30px">现任成员</h1>
+      <div class="home-personnel-line" style="width: 85%;height:2px;background-color: black;margin: 10px auto; "></div>
+      <div class="personnel-column" >
+        <div class="home-personnel-info">
+          <img class="info-avator" :src="personnel[0].avator" />
+          <div class="info-name"></div>
+        </div>
+        <div class="home-personnel-info">
+          <img class="info-avator" :src="personnel[0].avator" />
+          <div class="info-name"></div>
+        </div>
+        <div class="home-personnel-info">
+          <img class="info-avator" :src="personnel[0].avator" />
+          <div class="info-name"></div>
+        </div>
+        <div class="home-personnel-info">
+          <img class="info-avator" :src="personnel[0].avator" />
+          <div class="info-name"></div>
         </div>
       </div>
     </div>
-    <div class="home-personnel"></div>
     <div class="home-footer"></div>
   </div>
 </template>
@@ -26,12 +42,35 @@ export default {
     return {
       urls: ['./static/e15d7dc6cb065c2e74f0a2faf26f8fb0.jpg', './static/QQ图片20210113185921.png', './static/thumb-1920-1107572.png'],
       descriptions: ['1', '2', '3'],
-      articles: []
+      articles: [],
+      personnel: [
+        {
+          name: 'Decision',
+          avator: 'https://avatars0.githubusercontent.com/u/25838809?s=460&v=4'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
+  .info-avator{
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    border: solid 0.5px;
+    border-color: #d6d3d3;
+    margin: 20px 90px;
+    -webkit-transition:-webkit-transform 2s;
+  }
 
+  .info-avator:hover {
+    -webkit-transform: rotate(360deg);
+  }
+
+  .personnel-column {
+    display: flex;
+    justify-content: center;
+  }
 </style>
