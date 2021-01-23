a87229beb93ca7333d360ba86b611b0949f7b723
decision2016 <787362202@qq.com>
2021-01-23 23:22:17 +0800

    Update Login.vue

--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -1,5 +1,25 @@
 <template>
-<div></div>
+  <div class="login-page" style="min-height: 47vw;padding-top: 10vw;background: #fbfbfb;">
+    <!--div class="login-bg"></div-->
+    <div class="card login-card" >
+      <div class="card-body">
+        <div class="card-title">
+          <h5 style="text-align: left;margin: 1vw 0;">管理员登录</h5>
+        </div>
+        <form>
+          <div class="form-floating mb-4">
+            <input type="text" class="form-control" id="usernameInput">
+            <label for="usernameInput">Username</label>
+          </div>
+          <div class="form-floating mb-4">
+            <input type="text" class="form-control" id="passwordInput">
+            <label for="passwordInput">Password</label>
+          </div>
+          <button class="btn btn-primary" style="float: right">登录</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +29,17 @@ export default {
 </script>
 
 <style scoped>
+.login-bg {
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+  height: 100%;
+  background: #fbfbfb;
+}
 
+  .login-card {
+    margin: 0vw 37vw;
+    position: relative;
+    z-index: 1000;
+  }
 </style>
